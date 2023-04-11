505bdf72d590ec6139be28dd274f30a2f2199ebe
Iain Roach (RIT Student) <ijr8454@rit.edu>
2023-04-11 10:17:53 -0400

    adasfasdf

---
+++ Roach-I-wc-2/src/myBookmark.js
@@ -0,0 +1,78 @@
+const template = document.createElement("template");
+        template.innerHTML = `
+            <style>
+                :host {
+                    display: inline-block;
+                    background-color: #ddd;
+                }
+                span {
+                    color: #F76092;
+                    font-variant: small-caps;
+                    font-weight: bolder;
+                    font-family: sans-serif;
+                }
+            </style>
+            <span><a href="">???</a></span>
+        `;
+
+        class MyBookmark extends HTMLElement {
+            // called when the component is first created, but before it is added to the DOM
+            constructor() {
+                super();
+                this._text = "RIT";
+                this._url = "https://www.rit.edu/";
+                this._comments ="No Comments found";
+
+                // Attach a showdow DOM tree to this instance - this creates `.shadowRoot` for us
+                this.attachShadow({ mode: "open" });
+
+                // Clone `template and append it
+                this.shadowRoot.appendChild(template.content.cloneNode(true));
+            }
+
+            // tell the component what attributes to "watch"
+            static get observedAttributes() {
+                return ["data-text", "data-url", "data-comments"];
+            }
+
+            // ** lifecycle events **
+
+            // called when the component is inserted into the DOM
+            connectedCallback() {
+                this.render();
+            }
+
+            // this method is invoked each time one of the component's "watched" attributes changes
+            attributeChangedCallback(attributeName, oldValue, newValue) {
+                console.log(attributeName, oldValue, newValue);
+                if (oldValue === newValue) return;
+                if (attributeName == "data-text") {
+                    this._text = newValue;
+                }
+                if (attributeName == "data-url") {
+                    this._url = newValue;
+                }
+                if (attributeName == "data-comments") {
+                    this._comments = newValue;
+                }
+                this.render();
+            }
+
+            // helper method
+            render() {
+                //this.innerHTML = `<span><a href="${this._url}">${this._text}</a></span>`;
+                // Is the template loaded?
+                let a = this.shadowRoot.querySelector("a");
+
+                // if so, update the shadowDom
+                if(a) {
+                    a.href = this._url;
+                    a.textContent = this._text;
+                    a.title = this._comments;
+                }
+            }
+        }
+
+        customElements.define('my-bookmark', MyBookmark);
+
+        export {MyBookmark}; 
